79605494f79332f819137ca709089a0afc989a6f
manyatripathi <49054370+manyatripathi@users.noreply.github.com>
2019-05-30 12:38:47 +0530

    Create HeadlessChromeDemo.java

---
+++ src/test/java/in/javahome/myweb/controller/HeadlessChromeDemo.java
@@ -0,0 +1,29 @@
+package demo;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class HeadlessChromeDemo
+{
+    public static void main(String args[])
+    {
+        test();
+    }
+    public static void test(String args[])
+    {
+        System.setProperty("ebdriver.chrome.driver","/usr/bin/chromedriver");
+        
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--headless")
+        
+        WebDriver driver = new ChromeDriver(options);
+        
+        driver.get("https:google.com");
+        System.out.println(driver.getTitle());
+        driver.findElement(By.name("q")).sendKeys("Automation");
+        driver.findElement(By.name("btnK")).sendKeys(Keys.RETURN);
+        driver.close();
+        driver.quit();
+        System.out.println("Completed");
+    }
+}
