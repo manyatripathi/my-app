c2c52d71f3f7a311d76b18833cd39b80511a6988
manyatripathi <49054370+manyatripathi@users.noreply.github.com>
2019-05-30 15:16:02 +0530

    Update HeadlessChromeDemoIT.java

--- src/test/java/in/javahome/myweb/controller/HeadlessChromeDemoIT.java
+++ src/test/java/in/javahome/myweb/controller/HeadlessChromeDemoIT.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class HeadlessChromeDemoTest
+public class HeadlessChromeDemoIT
 {
     public static void main(String args[])
     {
