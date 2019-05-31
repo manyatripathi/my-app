2a4e9e71db2a9e1103072f09851273840df6dfdc
manyatripathi <49054370+manyatripathi@users.noreply.github.com>
2019-05-31 15:41:14 +0530

    Update HeadlessChromeDemoIT.java

--- src/test/java/in/javahome/myweb/controller/HeadlessChromeDemoIT.java
+++ src/test/java/in/javahome/myweb/controller/HeadlessChromeDemoIT.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import static org.junit.Assert.*;				
+import org.junit.Test;
+
 public class HeadlessChromeDemoIT
 {
     public static void main(String args[])
@@ -18,15 +21,16 @@ public class HeadlessChromeDemoIT
         
         ChromeOptions options=new ChromeOptions();
         options.addArguments("--headless");
+        options.addArguments("chrome.switches","--disable-extensions");
         
         WebDriver driver = new ChromeDriver(options);
+      
         
         driver.get("https:google.com");
         System.out.println(driver.getTitle());
-        driver.findElement(By.name("q")).sendKeys("Automation");
-        driver.findElement(By.name("btnK")).sendKeys(Keys.RETURN);
+        Assert.assertEquals(driver.getTitle(),"Google");	
         driver.close();
         driver.quit();
-        System.out.println("Completed");
+        
     }
 }
